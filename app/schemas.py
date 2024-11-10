23f6eca3a6c9b0f81192c909914899c16c2ee2e4
melihkale <mlh.58@hotmail.com>
2024-11-10 21:53:37 +0300

    API endpoint added

---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class APICreate(BaseModel):
+    name: str
+    url: str
+    method: str
+    params : Optional[str] = None
+    cookie : Optional[str] = None
